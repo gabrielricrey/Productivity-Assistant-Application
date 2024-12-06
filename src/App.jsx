e6ea8335ac0997bd2f079e4e6c8f48b7c124933f
Gabriel Richter Reynoso <garey91@gmail.com>
2024-12-06 15:44:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Routes, Route, Link} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Productivity Assistant App
+      </h1>
+    </>
+  )
+}
+
+export default App
