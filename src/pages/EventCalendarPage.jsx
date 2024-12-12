cfa355b840e1a48359374d082fbd77616a4f3d77
Gabriel Richter Reynoso <garey91@gmail.com>
2024-12-12 14:42:15 +0100

    Sort events and live update on status

--- src/pages/EventCalendarPage.jsx
+++ src/pages/EventCalendarPage.jsx
@@ -4,11 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 
 const EventCalendarPage = () => {
 
-    let [user, setUser] = useState(null); // Ta bort troligtvis
     let [events,setEvents] = useState(JSON.parse(localStorage.getItem("events")) || []);
     let activeUser = JSON.parse(sessionStorage.getItem("user")).username;
 
-        
     let [eventTitle,setEventTitle] = useState("");
     let [eventStart, setEventStart] = useState("");
     let [eventEnd, setEventEnd] = useState("");
@@ -21,7 +19,12 @@ const EventCalendarPage = () => {
 
     useEffect(() => {
         if (sessionStorage.getItem("user")) {
-            setUser(JSON.parse(sessionStorage.getItem("user")));
+
+            const interval = setInterval(() => {
+                setCurrentDateTime(new Date());
+              }, 60000);
+
+              return () => clearInterval(interval);
         } else {
             navigate('/')
         }
@@ -29,7 +32,6 @@ const EventCalendarPage = () => {
 
     useEffect(() => {
         localStorage.setItem("events", JSON.stringify(events));
-        console.log('CHANGED!')
     }, [events])
 
     function createEvent(event) {
@@ -97,12 +99,53 @@ const EventCalendarPage = () => {
                 <br />
                 <input type="datetime-local" value={eventEnd} min={eventStart} onChange={(e) => setEventEnd(e.target.value)}/>
                 <br />
-                <button onClick={() => {editMode? saveChanges() : createEvent({eventTitle,eventStart,eventEnd,userid: user.username})}}>{editMode ? "Save Changes" : "Create Event"}</button>
+                <button onClick={() => {editMode? saveChanges() : createEvent({eventTitle,eventStart,eventEnd,userid: activeUser})}}>{editMode ? "Save Changes" : "Create Event"}</button>
             </div>
             <div>
                 <h3>Events</h3>
                 <ul className="eventCardContainer">
-                    {events && events.filter(event => event.userid === activeUser).map(event => <li className="eventCard"><h3>{event.eventTitle}</h3><p>Start: {event.eventStart}</p><p>Slut: {event.eventEnd}</p> <button onClick={() => editEvent(event.id)}>Edit</button><button onClick={() => deleteEvent(event.id)}> X </button></li> )}
+                    {events && events
+                    .filter(event => event.userid === activeUser)
+                    .sort((a, b) => {
+                        const dateAStart = new Date(a.eventStart);
+                        const dateBStart = new Date(b.eventStart);
+                        
+                        if (dateAStart - dateBStart !== 0) {
+                            return dateAStart - dateBStart;
+                        }
+                        
+                        const dateBEnd = new Date(b.eventEnd);
+                        const dateAEnd = new Date(a.eventEnd);
+                        
+                        return dateAEnd - dateBEnd;
+                      })
+                    .map((event,i) => {
+
+                        let eventStart = new Date(event.eventStart);
+                        let eventEnd = new Date(event.eventEnd);
+
+                        eventStart.setSeconds(0,0)
+                        eventEnd.setSeconds(0,0)
+
+                        if(eventEnd < currentDateTime) {
+                            event.className = 'event-finished';
+                        } else if (eventStart < currentDateTime && currentDateTime < eventEnd){
+                            event.className ='event-ongoing'
+                        } else {
+                            event.className = '';
+                        }
+
+                        return (
+                            <li className={`${event.className} eventCard`} key={i}>
+                            <h3>{event.eventTitle}</h3>
+                            <p>Start: {event.eventStart}</p>
+                            <p>Slut: {event.eventEnd}</p> 
+                            <button onClick={() => editEvent(event.id)}>Edit</button>
+                            <button onClick={() => deleteEvent(event.id)}> X </button>
+                        </li>
+                        )
+
+                         })}
                 </ul>
             </div>
         </div>
